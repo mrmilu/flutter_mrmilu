15773ba6ccc944f27565bdbbb9d9d56c261fc9d6
Matias de Andrea <mdeandrea@mrmilu.com>
2022-06-10 08:50:56 +0200

    fix(root): Export http overrides

--- lib/flutter_mrmilu.dart
+++ lib/flutter_mrmilu.dart
@@ -8,6 +8,7 @@ export 'src/services/dio_rest_service.dart';
 export 'src/utils/extensions/date_time_extension.dart';
 export 'src/utils/extensions/int_extension.dart';
 export 'src/utils/extensions/string_extension.dart';
+export 'src/utils/http_overrides_utils.dart';
 export 'src/validators/date_validators.dart';
 export 'src/validators/document_validators.dart';
 export 'src/validators/password_validators.dart';
