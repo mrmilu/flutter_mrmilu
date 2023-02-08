2f14deaeefdc3c9dfd34c4ac1081ff45d2b1b576
Guido Ceraso <gceraso@mrmilu.com>
2023-02-08 14:51:05 +0100

    fix: fixed custom versioner

--- custom_version_updater.js
+++ custom_version_updater.js
@@ -1,9 +1,9 @@
   module.exports.readVersion = function (contents) {
-    const pubspecVersion = contents.match(/version: (\d\.\d\.\d)/g)[0].match(/(\d\.\d\.\d)/g)[0];
+    const pubspecVersion = contents.match(/version: (\d+\.\d+\.\d+)/g)[0].match(/(\d+\.\d+\.\d+)/g)[0];
     return pubspecVersion;
   };
 
   module.exports.writeVersion = function (contents, version) {
-    const newContent = contents.replace(/version: (\d\.\d\.\d)/g, "version: " + version)
+    const newContent = contents.replace(/version: (\d+\.\d+\.\d+)/g, "version: " + version)
     return newContent;
   };
